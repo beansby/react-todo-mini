6bbea36626f53d66b2d0f767142b06dd46d463a4
beansby <beanswebdev@gmail.com>
2022-11-17 20:33:18 +0900

    todo input form

---
+++ src/components/TodoForm.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function TodoForm ({value, onChange, onCreate, onKeyPress}){
+    return(
+        <div className="form">
+            {/* input 내용, 버튼 클릭시 실행 함수, 키 입력시 실행 함수 */}
+            <input value={value} placeholder="오늘 할 일을 입력하세요"
+                   onChange={onChange} onKeyPress={onKeyPress}/>
+            
+            {/* 버튼 클릭시 실행 함수 */}
+            <div className="create-button" onClick={onCreate}>
+                추가
+            </div>
+
+        </div>
+    );
+}
+
+export default TodoForm;
